48c8f439be1053feb5535bc48fba62e61d925364
novoforce <ashish.space5@gmail.com>
2022-02-23 22:17:22 +0530

    omr added

--- classical_cv/11_contour_detection.py
+++ classical_cv/11_contour_detection.py
@@ -55,6 +55,80 @@ def detect_contours2(image):
     cv2.drawContours(image, contours,-1,(0,0,255),4)
     display(image)
 
+def detect_contours_area_sort(image):
+
+    #convert image to gray Scale
+    image_g= cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+    #canny edges
+    edged= cv2.Canny(image_g,50,200)
+    # display(edged)
+
+    #detect contours from edges
+    contours, hierarchy = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    print("total contours:> ", len(contours))
+
+    #sorting the contours based on area
+    sorted_contours = sorted(contours, key=cv2.contourArea, reverse=True) #descending order
+
+    #plotting the contours
+    # RED,GREEN,BLUE,BLACK
+    color_dict= {'0':(255,0,0), '1':(0,255,0), '2':(0,0,255), '3':(0,0,0)}
+    for i,c in enumerate(sorted_contours):
+        cv2.drawContours(image, [c], -1, color_dict[str(i)], 6)
+    display(image)
+
+
+def detect_contours_coord_sort(image):
+    #convert image to gray Scale
+    image_g= cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    #canny edges
+    edged= cv2.Canny(image_g,50,200)
+    # display(edged)
+    #detect contours from edges
+    contours, hierarchy = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    print("total contours:> ", len(contours))
+
+    #computing the center of mass of each contours
+    for c in contours:
+        #calculate the moments
+        M = cv2.moments(c)
+        cx = int(M['m10'] / M['m00'])
+        cy = int(M['m01'] / M['m00'])
+        cv2.circle(image,(cx,cy), 10, (0,0,0), -1)
+    # display(image)
+
+    #sorting function
+    def x_cord_contour(contours):
+        M = cv2.moments(contours)
+        return (int(M['m10']/M['m00']))
+
+    def y_cord_contour(contours):
+        M = cv2.moments(contours)
+        return (int(M['m01']/M['m00']))
+
+    sorted_contours = sorted(contours, key=x_cord_contour, reverse=True)#descending
+
+    color_dict= {'0':(255,0,0), '1':(0,255,0), '2':(0,0,255), '3':(0,0,0)}
+    for i,c in enumerate(sorted_contours):
+        cv2.drawContours(image, [c], -1, color_dict[str(i)], 6)
+        #draw ranks on the middle of the contours
+        x= x_cord_contour(c)
+        y= y_cord_contour(c)
+        cv2.putText(image, str(i+1), (x,y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 4)
+
+        #crop the contour shapes from the original image
+        # (x, y, w, h) = cv2.boundingRect(c)  
+        # cropped_contour = image[y:y + h, x:x + w]
+        # image_name = "output_shape_number_" + str(i+1) + ".jpg"
+        # cv2.imwrite(image_name, cropped_contour)
+
+    display(image)
+
+
+
+
+
 
 
 
@@ -68,6 +142,17 @@ if __name__ == '__main__':
 
     imgp= r'images/hearts.png'
     img = cv2.cvtColor(cv2.imread(imgp),cv2.COLOR_BGR2RGB)
-    display(img)
+    # display(img)
     detect_contours2(img)
+
+    imgp = r'images/bunchofshapes.jpg'
+    img = cv2.cvtColor(cv2.imread(imgp),cv2.COLOR_BGR2RGB)
+    # display(img)
+    detect_contours_area_sort(img)
+
+    imgp = r'images/bunchofshapes.jpg'
+    img = cv2.cvtColor(cv2.imread(imgp),cv2.COLOR_BGR2RGB)
+    # display(img)
+    detect_contours_coord_sort(img)
+
 
